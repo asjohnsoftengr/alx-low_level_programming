abe09f1c1e63c9f9e53d0c243873ea629e1ce878
asjohnsoftengr <as.john@yahoo.co.uk>
2022-09-09 04:48:35 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-void main()
-	char
-{puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
